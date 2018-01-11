64bdf65535f3b6ce612dcde96d12bcc3b1d4ce50
Elif Demirel <elif.demirel@vestel.com.tr>
2018-01-11 08:54:38 +0300

    Added simple java file

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        String hellomsg = "hello world";
+        
+        System.out.println(hellomsg);
+    }
+}
