651618c300cae53ad1816117ca224064edaaf2f0
Elif Demirel <elif.demirel@vestel.com.tr>
2018-01-12 16:01:14 +0300

    Commiting on bugFix branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,17 @@
+import java.util.Date;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         
         String hellomsg = "hello world";
         
-        System.out.println(hellomsg);
+        
+        System.out.println(hellomsg + "\nDate and Time: " + displayDate());
+        ;
+    }
+    public static String displayDate() {
+        Date date = new Date();
+        return date.toString();
     }
 }
