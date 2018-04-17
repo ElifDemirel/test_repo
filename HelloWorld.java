89b89c114bc9f6c9e405178b0499d367af0b2314
Elif Demirel <elif.demirel@vestel.com.tr>
2018-04-17 09:34:39 +0300

    Revert "deneme"
    This reverts commit b02d41c4e70a63d6ebe818bbf184bfe7a3439b62.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {    
         
-        System.out.println(displayMsg() + "\ndate & time : " + displayDate());     
+        System.out.println(displayMsg() + "\ndate & time: " + displayDate());     
         
     }
     public static String displayDate() {
