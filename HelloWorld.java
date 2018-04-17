66a4314c764e94e4228c56a3499be1d0cce6ff1d
Elif Demirel <elif.demirel@vestel.com.tr>
2018-04-17 10:51:16 +0300

    abc

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {    
         
-        System.out.println(displayMsg() + "\ndate & time: " + displayDate());     
+        System.out.println(displayMsg() + "\ndate & time:cde " + displayDate());     
         
     }
     public static String displayDate() {
